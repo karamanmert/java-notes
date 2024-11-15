84e53fce9654ff71ed9c9bf799bc8d53234ded67
karamanmert <mertkrmn06@gmail.com>
2024-11-15 20:35:58 +0300

    Refactor

--- src/section_7/factory/Shape.java
+++
@@ -1,5 +0,0 @@
-package section_7.factory;
-
-public interface Shape {
-    void draw();
-}
--- src/section_7/factory/Square.java
+++
@@ -1,8 +0,0 @@
-package section_7.factory;
-
-public class Square implements Shape {
-    @Override
-    public void draw() {
-        System.out.println("Inside Square::draw() method.");
-    }
-}
